082b0a0b12644730b39954b8accd99ae00d53886
WHN138 <arifkurniawan1424@gmail.com>
2025-11-09 19:07:31 +0700

    pertemuan 5

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -4,20 +4,29 @@ class LuasLingkaran
     public const phi = 3.14;
     public int $jari;
 
+    public function __construct($isiJari = 1)
+    {
+        $this->jari = $isiJari;
+    }
+
     public function tampil($nama)
     {
         $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
-        echo "Ukuran Lingkaran dari {$nama} adalah : $rumus";
+        echo "Ukuran Lingkaran dari {$nama} adalah : $rumus ";
     }
 
     public static function testing()
     {
-        echo "ini dari static";
+        echo "ini dari static ";
+    }
+
+    public function __destruct()
+    {
+        echo "dah ah cape pisan ";
     }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 16;
+$lingkaran = new LuasLingkaran(16);
 $lingkaran->tampil('kepala lu');
 
 LuasLingkaran::testing();
