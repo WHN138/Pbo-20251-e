e6aafeb47ae06f894d2c9767c52a8d86c3d569c8
WHN138 <arifkurniawan1424@gmail.com>
2025-11-09 19:11:06 +0700

    pertemuan 6

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,32 +1,32 @@
 <?php
+
+namespace App\Math;
+
 class LuasLingkaran
 {
     public const phi = 3.14;
-    public int $jari;
+    private int $jari;
 
     public function __construct($isiJari = 1)
     {
         $this->jari = $isiJari;
     }
 
-    public function tampil($nama)
+    public function tampil($nama = 'ban')
     {
-        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
-        echo "Ukuran Lingkaran dari {$nama} adalah : $rumus ";
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
     public static function testing()
     {
-        echo "ini dari static ";
+        echo "<br/>";
+        echo "ini dari static";
     }
 
     public function __destruct()
     {
-        echo "dah ah cape pisan ";
+        echo "<br/>";
+        echo "udah ah cape";
     }
 }
-
-$lingkaran = new LuasLingkaran(16);
-$lingkaran->tampil('kepala lu');
-
-LuasLingkaran::testing();
